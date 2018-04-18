b1f3a18426c63d55ba2bd4bb5c2f410f489deaf3
rvsathe <reshma30m@gmail.com>
2018-04-18 16:18:59 -0400

    Updating folder structure. Checking in files related to Login server-side validations and register user action.

--- src/bottle_server.py
+++ src/bottle_server.py
@@ -10,6 +10,7 @@ import sys
 import os
 import pymysql
 import simplejson as json
+import loginModule as loginMod
 
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
@@ -19,13 +20,13 @@ id_edit = 0
 
 @route('/<filepath:path>')
 def server_static(filepath):
+	print (dir_path)
 	return static_file(filepath, root=dir_path+'/view/')
     #return static_file(filepath, root='resources/')
 
 @bottle.get('/') 
 def present_signup():
-	return bottle.redirect("/login.html") 
-
+	return bottle.redirect("login.html") 
 
 @bottle.get('/edit') 
 def edit_page(): 
@@ -38,6 +39,7 @@ def edit_page():
 
 @bottle.post('/getPermits')
 def get_permits():
+	#db = pymysql.connect(host="localhost",port=3307,user="root",passwd="DBMasters<>123",db="ProjectDatabase")
 	db = pymysql.connect("localhost","root","DBMasters<>123","ProjectDatabase")
 	cursor = db.cursor()
 	cursor.execute("SELECT id_permit, applicant_name, action_type, category, desecription, work_type from Permit")
@@ -46,9 +48,10 @@ def get_permits():
 	print (id_edit)
 	return ret
 
-@bottle.post('/getPermitsWithFilter')
+@bottle.post('/getPermitsWithFilter/')
 def get_permitsWithfilter():
 	filter2 = request.json['filter'].lower()
+	#db = pymysql.connect(host="localhost",port=3307,user="root",passwd="DBMasters<>123",db="ProjectDatabase")
 	db = pymysql.connect("localhost","root","DBMasters<>123","ProjectDatabase")
 	cursor = db.cursor()
 	cursor.execute("SELECT id_permit, applicant_name, action_type, category, desecription, work_type from Permit where lower(category) like '%"+filter2+"%'")
@@ -62,6 +65,7 @@ def get_permitsWithfilter():
 def postResource():
 	data = request.json
 	id_permit = data['id_permit']
+	#db = pymysql.connect(host="localhost",port=3307,user="root",passwd="DBMasters<>123",db="ProjectDatabase")
 	db = pymysql.connect("localhost","root","DBMasters<>123","ProjectDatabase")
 	cursor = db.cursor()
 	cursor.execute("delete from Permit where id_permit="+str(id_permit))
@@ -80,6 +84,7 @@ def editResource():
 @bottle.post('/getPermitData')
 def getDataResource():
 	print ("getting Permit Data!")
+	#db = pymysql.connect(host="localhost",port=3307,user="root",passwd="DBMasters<>123",db="ProjectDatabase")
 	db = pymysql.connect("localhost","root","DBMasters<>123","ProjectDatabase")
 	cursor = db.cursor()
 	cursor.execute("SELECT id_permit, applicant_name, action_type, category, desecription, work_type from Permit where id_permit="+str(id_edit))
@@ -90,6 +95,7 @@ def getDataResource():
 @bottle.post('/createPermit')
 def createResource():
 	data = request.json['data']
+	#db = pymysql.connect(host="localhost",port=3307,user="root",passwd="DBMasters<>123",db="ProjectDatabase")
 	db = pymysql.connect("localhost","root","DBMasters<>123","ProjectDatabase")
 	cursor = db.cursor()
 	cursor.execute("insert into Permit(applicant_name, action_type, category, desecription, work_type) values ('"+data['applicant_name']+"', '"+data['action_type']+"', '"+data['category']+"', '"+data['description']+"', '"+data['work_type']+"')")
@@ -99,12 +105,33 @@ def createResource():
 @bottle.post('/editData')
 def editDataResource():
 	data = request.json['data']
+	#db = pymysql.connect(host="localhost",port=3307,user="root",passwd="DBMasters<>123",db="ProjectDatabase")
 	db = pymysql.connect("localhost","root","DBMasters<>123","ProjectDatabase")
 	cursor = db.cursor()
 	cursor.execute("update Permit set applicant_name='"+data['applicant_name']+"', action_type='"+data['action_type']+"', category='"+data['category']+"', desecription='"+data['description']+"', work_type='"+data['work_type']+"' where id_permit="+str(id_edit))
 	db.commit()
 	return {"result": "created"}
 
-	
+@bottle.post('/getUserTypes')
+def getUserTypes():
+    return loginMod.getUserTypes()
+
+
+@bottle.post('/loginAction')
+def performLogin():
+	print('Navigating to login Mod - perform login!!')
+	data = request.json
+	# Navigating to loginModule.py
+	return loginMod.performLogin(data)
+
+@bottle.post('/registerAction')
+def performRegistration():
+	print('Navigating to login Mod - register!!')
+	data = request.json
+	# Navigating to loginModule.py
+	return loginMod.registerUser(data)
+
+
 bottle.debug(True) 
 bottle.run(host='localhost', port=8080)
+#bottle.run(host='localhost', port=8090)
